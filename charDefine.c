82bcfe13e7bb5289c73b5ccd780a5366090e71d4
Mridul <40261046+Mehedi259@users.noreply.github.com>
2022-03-03 21:18:48 +0600

    Create charDefine.c

---
+++ charDefine.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    if (ch == 'a')
+    {
+        printf("vowel\n");
+    }
+    else if (ch == 'e')
+    {
+        printf("vowel\n");
+    }
+    else if (ch == 'i')
+    {
+        printf("Vowel\n");
+    }
+    else if (ch == 'o')
+    {
+        printf("vowel\n");
+    }
+    else if (ch == 'u')
+    {
+        printf("vowel\n");
+    }
+
+    else
+    {
+        printf("Consonant\n");
+    }
+    return 0;
+}
