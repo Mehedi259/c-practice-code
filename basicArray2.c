f535d91071af2145285c9a214d6d8f6fb4e990f1
Mehedi Hasan Mridul <40261046+Mehedi259@users.noreply.github.com>
2022-03-12 19:26:28 +0600

    Add files via upload

---
+++ basicArray2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num[] = {2, 3, 56, 23};
+    printf("the num[0] array element is %d\n" ,num[0]);
+    printf("the num[1] array element is %d\n" ,num[1]);
+    printf("the num[2] array element is %d\n" ,num[2]);
+    printf("the num[3] array element is %d\n" ,num[3]);
+
+    return 0;
+}
