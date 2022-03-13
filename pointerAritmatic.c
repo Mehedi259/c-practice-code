ee8035ef3ab38946ca508b7c3eb9facc1a7413ba
Mehedi Hasan Mridul <40261046+Mehedi259@users.noreply.github.com>
2022-03-13 21:25:04 +0600

    Update pointerAritmatic.c

--- pointerAritmatic.c
+++ pointerAritmatic.c
@@ -4,8 +4,13 @@ int main()
 {
     int i = 18;
     int *ptr = &i;
+    chr c = 21;
+    chr *plo = &c;
     printf("the value of ptr is %u\n", ptr);
     ptr++;
-    printf("the secend location is %u", ptr);
+    printf("the secend location is %u\n", ptr);
+    printf("the value of plo is %u"\n, plo);
+    plo++;
+    printf("the secend value of plo is %u\n", plo);
     return 0;
 }
